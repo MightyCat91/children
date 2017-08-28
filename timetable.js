dcff27cdd5fb621181e6ee9a217b66150eefb372
muzhilkin <muzhilkin@krista.ru>
2017-08-28 17:16:40 +0300

    #commit

--- timetable.js
+++ timetable.js
@@ -1,5 +1,7 @@
 jQuery(function ($) {
-    var eventCotainer = $('.event__container');
+    var eventCotainer = $('.event__container'),
+        days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+
 
     $('.timetable__table tr').each(function () {
         var rowHeight = $(this).innerHeight();
@@ -29,8 +31,8 @@ jQuery(function ($) {
                 eventDay = $(this).data('day'),
                 notEmptyEventContainer = eventCotainer.filter('[data-subject="' + eventName + '"][data-day="' + eventDay + '"]:first');
 
-            notEmptyEventContainer.height('fullHeight').next().addClass('hidden');
-            notEmptyEventContainer.prev().addClass('hidden');
+            notEmptyEventContainer.height('fullHeight').nextAll().addClass('hidden');
+            notEmptyEventContainer.prevAll().addClass('hidden');
 
             eventCotainer
                 .filter('[data-subject="' + eventName + '"][data-day="' + eventDay + '"]:not(:first)')
@@ -39,5 +41,20 @@ jQuery(function ($) {
         function () {
             eventCotainer.removeClass('hovered__event hidden');
         }
-    )
+    );
+
+    if($(window).width() <= 678) {
+        $.each(days, function (index, value) {
+            eventCotainer.filter('[data-day=' + value + ']').map(function (indx, element) {
+                var subjectName = $(element).find('.subject__name').text(),
+                    subjectTime = $(element).find('.hours__start').text() + ' - ' + $(element).find('.hours__end').text(),
+                    dayContainer = $('.small__table__day__subjects.' + value),
+                    newSubject = dayContainer.find('.small__table__subject__wrapper:first-child').clone();
+
+                newSubject.find('.small__subject__name').text(subjectName);
+                newSubject.find('.small__subject__hours').text(subjectTime);
+                dayContainer.append(newSubject.html());
+            });
+        });
+    }
 });
