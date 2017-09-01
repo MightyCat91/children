c453c5317d14a4a10bef1328bf64d9b80e287068
muzhilkin <muzhilkin@krista.ru>
2017-09-01 17:04:44 +0300

    #fix

--- timetable.js
+++ timetable.js
@@ -25,23 +25,23 @@ jQuery(function ($) {
         $('.age__tabs').addClass('hidden');
     });
 
-    eventCotainer.hover(
-        function () {
-            var eventName = $(this).data('subject'),
-                eventDay = $(this).data('day'),
-                tabletContainer = eventCotainer.closest('.table__wrapper'),
-                notEmptyEventContainer = tabletContainer.find('.event__container[data-subject="' + eventName + '"][data-day="' + eventDay + '"]:first');
-
-            notEmptyEventContainer.height('fullHeight').nextAll().addClass('hidden');
-            notEmptyEventContainer.prevAll().addClass('hidden');
-
-            tabletContainer.find('.event__container[data-subject="' + eventName + '"][data-day="' + eventDay + '"]:not(:first)')
-                .addClass('hovered__event');
-        },
-        function () {
-            eventCotainer.removeClass('hovered__event hidden');
-        }
-    );
+    // eventCotainer.hover(
+    //     function () {
+    //         var eventName = $(this).data('subject'),
+    //             eventDay = $(this).data('day'),
+    //             tabletContainer = eventCotainer.closest('.table__wrapper'),
+    //             notEmptyEventContainer = tabletContainer.find('.event__container[data-subject="' + eventName + '"][data-day="' + eventDay + '"]:first');
+    //
+    //         notEmptyEventContainer.height('fullHeight').nextAll().addClass('hidden');
+    //         notEmptyEventContainer.prevAll().addClass('hidden');
+    //
+    //         tabletContainer.find('.event__container[data-subject="' + eventName + '"][data-day="' + eventDay + '"]:not(:first)')
+    //             .addClass('hovered__event');
+    //     },
+    //     function () {
+    //         eventCotainer.removeClass('hovered__event hidden');
+    //     }
+    // );
 
     if($(window).width() <= 678) {
         generateMobileTable();
@@ -57,12 +57,14 @@ jQuery(function ($) {
         $.each(days, function (index, value) {
             var dayContainer = $('.small__table__day__subjects.' + value);
             dayContainer.children(':not(.hidden)').remove();
-            $('.table__wrapper.active').find('.event__container:not(.empty)[data-day=' + value + ']').map(function (indx, element) {
+            $('.table__wrapper.active').find('.' + value + ' .event__container').map(function (indx, element) {
                 var subjectName = $(element).find('.subject__name').text(),
+                    subjectAge = $(element).find('.subject__age').text(),
                     subjectTime = $(element).find('.hours__start').text() + ' - ' + $(element).find('.hours__end').text(),
                     newSubject = dayContainer.find('.small__table__subject__wrapper:first-child').clone().removeClass('hidden');
 
                 newSubject.find('.small__subject__name').text(subjectName);
+                newSubject.find('.small__subject__age').text(subjectAge);
                 newSubject.find('.small__subject__hours').text(subjectTime);
                 dayContainer.append(newSubject[0].outerHTML);
             });
